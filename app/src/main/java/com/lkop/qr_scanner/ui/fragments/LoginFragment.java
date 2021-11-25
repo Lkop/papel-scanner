e8a29058dc6b1737275a0c27f58445b8f56873db
Loukas Kopanias <37883287+Lkop@users.noreply.github.com>
2021-11-25 22:22:11 +0200

    Correcting LoginFragment and its layout

--- app/src/main/java/com/lkop/qr_scanner/ui/fragments/LoginFragment.java
+++ app/src/main/java/com/lkop/qr_scanner/ui/fragments/LoginFragment.java
@@ -1,72 +1,58 @@
 package com.lkop.qr_scanner.ui.fragments;
 
 import android.os.Bundle;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
-import com.lkop.qr_scanner.network.AsyncPostDataToURL;
-import com.lkop.qr_scanner.network.AsyncResults;
-import com.lkop.qr_scanner.constants.URLS;
+import androidx.fragment.app.Fragment;
 import com.example.lkop.qr_scanner.R;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
+import com.lkop.qr_scanner.constants.URLS;
+import com.lkop.qr_scanner.network.AsyncHttp;
+import com.lkop.qr_scanner.network.AsyncResultsCallbackInterface;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class LoginFragment extends Fragment {
 
-    private Button login_btn;
-    private EditText email_editText, password_editText;
-    private TextView message_box;
-
+    private View view;
+    private Button login_button;
+    private EditText email_edittext, password_edittext;
+    private TextView message_textview;
     private String next_fragment_name;
 
     @Override
-    public void onCreate(Bundle savedInstanceState){
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         Bundle args = getArguments();
         next_fragment_name = args.getString("next_fragment_name");
-
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_login, container, false);
 
-        View view = inflater.inflate(R.layout.fragment_login, container, false);
+        email_edittext = (EditText)view.findViewById(R.id.login_email);
+        password_edittext = (EditText)view.findViewById(R.id.login_password);
+        message_textview = (TextView)view.findViewById(R.id.message_box);
 
-        //AdjustResize Programmatically
-        getActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
-
-        email_editText = (EditText)view.findViewById(R.id.login_email);
-        password_editText = (EditText)view.findViewById(R.id.login_password);
-        message_box = (TextView)view.findViewById(R.id.message_box);
-
-
-        //Create Button Event
-        login_btn = (Button)view.findViewById(R.id.login_btn);
-        login_btn.setOnClickListener(new View.OnClickListener() {
+        login_button = (Button)view.findViewById(R.id.login_btn);
+        login_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String email_string = email_edittext.getText().toString();
+                String password_string = password_edittext.getText().toString();
 
-                String email_string = email_editText.getText().toString();
-                String password_string = password_editText.getText().toString();
-
-                if(email_string.isEmpty() || password_string.isEmpty()){
+                if(email_string.isEmpty() || password_string.isEmpty()) {
                     showErrorMessage("Γράψτε Email και Κωδικό");
                 }else{
-
-                    //Hide messsage
-                    message_box.setVisibility(View.GONE);
+                    message_textview.setVisibility(View.GONE);
 
                     //Disable Button, Email and Password
                     disableUIComponents();
@@ -76,150 +62,82 @@ public class LoginFragment extends Fragment {
                     user_data.put("email", email_string + "@uop.gr");
                     user_data.put("password", password_string);
 
-//                    new AsyncPostDataToURL(DefineURLS.VERIFY_USER, user_data).run(
-//                        new AsyncResults() {
-//                            @Override
-//                            public void taskResultsObject(Object results) {
-//
-//                                String r = (String)results;
-//
-//                                if (results == null || r.isEmpty()) {
-//
-//                                    showErrorMessage("Λάθος Στοιχεία");
-//                                    enableUIComponents();
-//                                }else{
-//
-//                                    try {
-//                                        JSONObject obj = new JSONObject(r);
-//                                        String email = obj.getString("email");
-//
-//                                        //FIX THIS BLOCK (NO NEED)
-//                                        //////////////////////////////////////////////////////
-//                                        //////////////////////////////////////////////////////
-//                                        if(email.equals(email_string)){
-//
-//                                            Fragment NextFragment = null;
-//
-//                                            if(next_fragment_name.equals("FragmentCreateClassroom")){
-//                                                NextFragment = new FragmentCreateClassroom();
-//                                            }else if(next_fragment_name.equals("FragmentAllClassrooms")){
-//                                                NextFragment = new FragmentAllClassrooms();
-//                                            }
-//
-//                                            try {
-//                                                getActivity().getSupportFragmentManager().popBackStack();
-//
-//                                                getActivity().getSupportFragmentManager().beginTransaction()
-//                                                        //.replace(R.id.fragment_create_classroom, FragmentCreateClassroom)
-//                                                        .replace(((ViewGroup)getView().getParent()).getId(), NextFragment)
-//                                                        .addToBackStack(null)
-//                                                        .commit();
-//
-//                                            }catch (NullPointerException e){
-//                                                e.printStackTrace();
-//                                            }
-//                                        }
-//                                        //////////////////////////////////////////////////////
-//                                        //////////////////////////////////////////////////////
-//
-//                                    }catch(JSONException e){
-//                                        enableUIComponents();
-//                                    }
-//                                }
-//                            }
-//                        });
+                    new AsyncHttp().get(URLS.UOP_VERIFY_USER, user_data, new AsyncResultsCallbackInterface() {
+                        @Override
+                        public void onSuccess(String json_string) {
+                            if (json_string == null || json_string.isEmpty()) {
+                                showErrorMessage("Λάθος Στοιχεία");
+                                enableUIComponents();
+                            }else{
+                                String response_status = "", response_message = "";
+                                try {
+                                    ObjectMapper mapper = new ObjectMapper();
+                                    JsonNode jsonNode = mapper.readTree(json_string);
+                                    response_status = jsonNode.get("status").asText();
+                                    response_message = jsonNode.get("message").asText();
+                                }catch(IOException e){
+                                    e.printStackTrace();
+                                }
 
-                    new AsyncPostDataToURL(URLS.UOP_VERIFY_USER, user_data).run(
-                        new AsyncResults() {
-                            @Override
-                            public void taskResultsObject(Object results) {
-
-                                String r = (String)results;
-
-                                if (results == null || r.isEmpty()) {
-
+                                if(response_status.equals("success") && response_message.equals("verified OK")) {
+                                    Fragment NextFragment = null;
+                                    if(next_fragment_name.equals("FragmentCreateClassroom")) {
+                                        NextFragment = new CreateClassroomFragment();
+                                    }else if(next_fragment_name.equals("FragmentAllClassrooms")) {
+                                        NextFragment = new AllClassroomsFragment();
+                                    }
+                                    //open fragment (code run in background)
+                                    openFragmentMainThread(NextFragment);
+                                }else{
                                     showErrorMessage("Λάθος Στοιχεία");
                                     enableUIComponents();
-                                }else{
-
-                                    String response_status = "", response_message = "";
-
-                                    try {
-                                        ObjectMapper mapper = new ObjectMapper();
-
-                                        JsonNode jsonNode = mapper.readTree(r);
-
-                                        response_status = jsonNode.get("status").asText();
-                                        response_message = jsonNode.get("message").asText();
-
-                                    }catch(IOException e){ }
-
-                                    if(response_status.equals("success") && response_message.equals("verified OK")){
-
-                                        Fragment NextFragment = null;
-
-                                        if(next_fragment_name.equals("FragmentCreateClassroom")){
-                                            NextFragment = new CreateClassroomFragment();
-                                        }else if(next_fragment_name.equals("FragmentAllClassrooms")){
-                                            NextFragment = new AllClassroomsFragment();
-                                        }
-
-                                        //open fragment (code run in background)
-                                        openFragmentMainThread(NextFragment);
-
-                                    }else{
-                                        showErrorMessage("Λάθος Στοιχεία");
-                                        enableUIComponents();
-                                    }
                                 }
                             }
-                        });
+                        }
+
+                        @Override
+                        public void onFailure(Throwable t) {
+
+                        }
+                    });
                 }
             }
         });
         return view;
     }
 
-    private void enableUIComponents(){
-
+    private void enableUIComponents() {
         getActivity().runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                email_editText.setEnabled(true);
-                password_editText.setEnabled(true);
-                login_btn.setEnabled(true);
+                email_edittext.setEnabled(true);
+                password_edittext.setEnabled(true);
+                login_button.setEnabled(true);
             }
         });
     }
 
-    private void disableUIComponents(){
-        email_editText.setEnabled(false);
-        password_editText.setEnabled(false);
-        login_btn.setEnabled(false);
-
+    private void disableUIComponents() {
+        email_edittext.setEnabled(false);
+        password_edittext.setEnabled(false);
+        login_button.setEnabled(false);
     }
 
-    private void showErrorMessage(String msg){
-
-        final String tmp_msg = msg;
-
+    private void showErrorMessage(String message) {
         getActivity().runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                message_box.setText(tmp_msg);
-                message_box.setVisibility(View.VISIBLE);
+                message_textview.setText(message);
+                message_textview.setVisibility(View.VISIBLE);
             }
         });
     }
 
-    private void openFragmentMainThread(final Fragment fragment){
-
+    private void openFragmentMainThread(Fragment fragment) {
         getActivity().runOnUiThread(new Runnable() {
             @Override
             public void run() {
-
                 getActivity().getSupportFragmentManager().popBackStack();
-
                 getActivity().getSupportFragmentManager().beginTransaction()
                         //.replace(R.id.fragment_create_classroom, FragmentCreateClassroom)
                         .replace(((ViewGroup)getView().getParent()).getId(), fragment)
