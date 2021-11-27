3a5eec04eb399d18f17ff864f878d6627302e38a
Loukas Kopanias <37883287+Lkop@users.noreply.github.com>
2021-11-27 23:06:26 +0200

    Adding remember account option in LoginFragment

--- app/src/main/java/com/lkop/qr_scanner/ui/fragments/LoginFragment.java
+++ app/src/main/java/com/lkop/qr_scanner/ui/fragments/LoginFragment.java
@@ -1,13 +1,17 @@
 package com.lkop.qr_scanner.ui.fragments;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
 import androidx.fragment.app.Fragment;
+import androidx.preference.PreferenceManager;
 import com.example.lkop.qr_scanner.R;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -24,7 +28,10 @@ public class LoginFragment extends Fragment {
     private Button login_button;
     private EditText email_edittext, password_edittext;
     private TextView message_textview;
+    private CheckBox save_checkbox;
+    private String saved_email, saved_password;
     private String next_fragment_name;
+    private SharedPreferences shared_prefs;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -32,6 +39,10 @@ public class LoginFragment extends Fragment {
 
         Bundle args = getArguments();
         next_fragment_name = args.getString("next_fragment_name");
+
+        shared_prefs = PreferenceManager.getDefaultSharedPreferences(getContext());
+        saved_email = shared_prefs.getString("PREF_KEY_SAVED_EMAIL", null);
+        saved_password = shared_prefs.getString("PREF_KEY_SAVED_PASSWORD", null);
     }
 
     @Override
@@ -41,6 +52,23 @@ public class LoginFragment extends Fragment {
         email_edittext = (EditText)view.findViewById(R.id.login_email);
         password_edittext = (EditText)view.findViewById(R.id.login_password);
         message_textview = (TextView)view.findViewById(R.id.message_box);
+        save_checkbox = (CheckBox)view.findViewById(R.id.save_checkbox);
+
+        save_checkbox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(!isChecked) {
+                    shared_prefs.edit().remove("PREF_KEY_SAVED_EMAIL").apply();
+                    shared_prefs.edit().remove("PREF_KEY_SAVED_PASSWORD").apply();
+                }
+            }
+        });
+
+        if(saved_email != null && saved_password != null) {
+            email_edittext.setText(saved_email);
+            password_edittext.setText(saved_password);
+            save_checkbox.setChecked(true);
+        }
 
         login_button = (Button)view.findViewById(R.id.login_btn);
         login_button.setOnClickListener(new View.OnClickListener() {
@@ -62,7 +90,7 @@ public class LoginFragment extends Fragment {
                     user_data.put("email", email_string + "@uop.gr");
                     user_data.put("password", password_string);
 
-                    new AsyncHttp().get(URLS.UOP_VERIFY_USER, user_data, new AsyncResultsCallbackInterface() {
+                    new AsyncHttp().post(URLS.UOP_VERIFY_USER, user_data, new AsyncResultsCallbackInterface() {
                         @Override
                         public void onSuccess(String json_string) {
                             if (json_string == null || json_string.isEmpty()) {
@@ -80,6 +108,11 @@ public class LoginFragment extends Fragment {
                                 }
 
                                 if(response_status.equals("success") && response_message.equals("verified OK")) {
+                                    if (save_checkbox.isChecked()) {
+                                        shared_prefs.edit().putString("PREF_KEY_SAVED_EMAIL", email_string).apply();
+                                        shared_prefs.edit().putString("PREF_KEY_SAVED_PASSWORD", password_string).apply();
+                                    }
+
                                     Fragment NextFragment = null;
                                     if(next_fragment_name.equals("FragmentCreateClassroom")) {
                                         NextFragment = new CreateClassroomFragment();
