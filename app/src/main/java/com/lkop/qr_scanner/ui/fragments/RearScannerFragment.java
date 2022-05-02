c2a53f623871459ea65dffc5b39647ef1dcd1665
Loukas Kopanias <37883287+Lkop@users.noreply.github.com>
2022-05-02 03:29:09 +0300

    Fixing RearScanner to scan both qr and text

--- app/src/main/java/com/lkop/qr_scanner/ui/fragments/RearScannerFragment.java
+++ app/src/main/java/com/lkop/qr_scanner/ui/fragments/RearScannerFragment.java
@@ -40,6 +40,7 @@ import com.google.mlkit.vision.barcode.BarcodeScannerOptions;
 import com.google.mlkit.vision.barcode.BarcodeScanning;
 import com.google.mlkit.vision.barcode.common.Barcode;
 import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.text.Text;
 import com.google.mlkit.vision.text.TextRecognition;
 import com.google.mlkit.vision.text.TextRecognizer;
 import com.google.mlkit.vision.text.latin.TextRecognizerOptions;
@@ -56,6 +57,11 @@ public class RearScannerFragment extends Fragment {
     private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
     private BarcodeScanner barcode_scanner;
     private TextRecognizer text_recognizer;
+    private Bitmap bitmap;
+    private Canvas canvas;
+    private Rect last_qr_rect, last_text_rect;
+    private boolean onetime_flag = false;
+    private boolean is_bitmap_full = false;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -63,6 +69,13 @@ public class RearScannerFragment extends Fragment {
 
         camera_previewview = (PreviewView) view.findViewById(R.id.camera_previewview);
         camera_imageview = (ImageView) view.findViewById(R.id.camera_imageview);
+
+        //Initialize drawable image
+        bitmap = Bitmap.createBitmap(720, 1280, Bitmap.Config.ARGB_8888);
+        canvas = new Canvas(bitmap);
+
+        last_qr_rect = new Rect();
+        last_text_rect = new Rect();
         //test_textview = (TextView) view.findViewById(R.id.test_textview);
 
 //        BarcodeScannerOptions options =
@@ -131,6 +144,15 @@ public class RearScannerFragment extends Fragment {
                             .addOnSuccessListener(new OnSuccessListener<List<Barcode>>() {
                                 @Override
                                 public void onSuccess(List<Barcode> barcodes) {
+                                    if (onetime_flag == true){
+                                        return;
+                                    }
+
+//                                    if(is_bitmap_full) {
+//                                        resetBitmap();
+//                                        is_bitmap_full = false;
+//                                    }
+
                                     for (Barcode barcode: barcodes) {
                                         Rect bounds = new Rect(barcode.getBoundingBox());
                                         Point[] corners = barcode.getCornerPoints();
@@ -141,32 +163,23 @@ public class RearScannerFragment extends Fragment {
 
                                         int valueType2 = barcode.getFormat();
 
-                                        int width = camera_imageview.getWidth();
-                                        int height = camera_imageview.getHeight();
-                                        int rot = imageProxy.getImageInfo().getRotationDegrees();
-
                                         if (barcode.getFormat() == Barcode.FORMAT_QR_CODE && barcode.getValueType() == Barcode.TYPE_TEXT) {
-                                            Bitmap bitmap = Bitmap.createBitmap(720, 1280, Bitmap.Config.ARGB_8888);
-                                            Canvas canvas = new Canvas(bitmap);
-                                            Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-                                            paint.setColor(Color.RED);
+//
+//                                            Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+//                                            paint.setColor(Color.RED);
+//                                            paint.setStyle(Paint.Style.STROKE);
+//                                            paint.setStrokeWidth(4);
+//
+//                                            RectF rect = new RectF(barcode.getBoundingBox());
+//                                            canvas.drawRect(bounds.left-10, bounds.top-10, bounds.right+10, bounds.bottom+10, paint);
+//                                            camera_imageview.setImageBitmap(bitmap);
 
-                                            RectF rect = new RectF(barcode.getBoundingBox());
-//                                            float x0 = translateX(rect.left);
-//                                            float x1 = translateX(rect.right);
-//                                            rect.left = min(x0, x1);
-//                                            rect.right = max(x0, x1);
-//                                            rect.top = translateY(rect.top);
-//                                            rect.bottom = translateY(rect.bottom);
-//                                            canvas.drawRect(bounds, paint);
-                                            canvas.drawRect(bounds.left-10, bounds.top-10, bounds.right+10, bounds.bottom+10, paint);
-                                            //canvas.drawRect(bounds, paint);
-
-                                            //canvas.drawRect(bounds, paint);
-                                            camera_imageview.setImageBitmap(bitmap);
+                                            last_qr_rect = barcode.getBoundingBox();
+                                            drawPreviewRects(last_qr_rect, last_text_rect);
 
                                             String ssid = barcode.getRawValue();
                                             //test_textview.setText(ssid);
+                                            onetime_flag = true;
                                         }
                                     }
                                 }
@@ -184,22 +197,93 @@ public class RearScannerFragment extends Fragment {
                                 }
                             });
 
-//                    Task<Text> text_result = text_recognizer.process(image).addOnSuccessListener(new OnSuccessListener<Text>() {
-//                        @Override
-//                        public void onSuccess(Text visionText) {
+                    Task<Text> text_result = text_recognizer.process(image).addOnSuccessListener(new OnSuccessListener<Text>() {
+                        @Override
+                        public void onSuccess(Text visionText) {
+                            if (onetime_flag == false){
+                                return;
+                            }
+
+                            String resultText = visionText.getText();
+                            for (Text.TextBlock block : visionText.getTextBlocks()) {
+                                String blockText = block.getText();
+                                Point[] blockCornerPoints = block.getCornerPoints();
+                                Rect blockFrame = block.getBoundingBox();
+
+
+
+                                for (Text.Line line : block.getLines()) {
+                                    String lineText = line.getText();
+                                    Point[] lineCornerPoints = line.getCornerPoints();
+                                    Rect lineFrame = line.getBoundingBox();
+
+                                    int am_check = lineText.indexOf("A.M.:");
+                                    String[] am = new String[19 - 6];
+
+                                    if (am_check != -1) {
+
+//                                        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+//                                        paint.setColor(Color.WHITE);
+//                                        paint.setStyle(Paint.Style.STROKE);
+//                                        paint.setStrokeWidth(4);
+//
+//                                        Rect bounds = new Rect(lineFrame);
+//
+//                                        canvas.drawRect(bounds.left-10, bounds.top-10, bounds.right+10, bounds.bottom+10, paint);
+//                                        camera_imageview.setImageBitmap(bitmap);
+
+                                        last_text_rect = line.getBoundingBox();
+                                        drawPreviewRects(last_qr_rect, last_text_rect);
+
+
+
+//                                        for (int i = 0; i < 19 - 6; i++) {
+//                                            am[i] = lineText.substring(am_check + 6 + i, am_check + 6 + i + 1);
+//                                        }
+//
+//                                        boolean numeric = true;
+//                                        for (int i = 0; i < 19 - 6; i++) {
+//                                            try {
+//                                                Integer.parseInt(am[i]);
+//                                            } catch (NumberFormatException e) {
+//                                                numeric = false;
+//                                            }
+//
+//                                        }
+                                        onetime_flag = false;
+                                        //is_bitmap_full = true;
+                                    }
+
+
+//
+
+//                                    for (Text.Element element : line.getElements()) {
+//                                        String elementText = element.getText();
+//                                        Point[] elementCornerPoints = element.getCornerPoints();
+//                                        Rect elementFrame = element.getBoundingBox();
+//                                    }
+
+                                }
+                            }
+
 //                            String blockText = visionText.getText();
 //                            test_textview.setText(blockText);
 //                            //Toast.makeText(view.getContext(), blockText, Toast.LENGTH_SHORT).show();
-//                            imageProxy.close();
-//                        }
-//                    })
-//                    .addOnFailureListener(new OnFailureListener() {
-//                        @Override
-//                        public void onFailure(@NonNull Exception e) {
-//                            e.printStackTrace();
-//                            imageProxy.close();
-//                        }
-//                    });
+                            imageProxy.close();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            e.printStackTrace();
+                            imageProxy.close();
+                        }
+                    }).addOnCompleteListener(new OnCompleteListener<Text>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Text> task) {
+                            imageProxy.close();
+                        }
+                    });;
                 }
 
             }
@@ -207,4 +291,29 @@ public class RearScannerFragment extends Fragment {
 
         Camera camera = cameraProvider.bindToLifecycle((LifecycleOwner)this, cameraSelector, imageAnalysis, preview);
     }
+
+//    private void resetBitmap() {
+//        bitmap = Bitmap.createBitmap(720, 1280, Bitmap.Config.ARGB_8888);
+//        canvas = new Canvas(bitmap);
+//    }
+
+    private void drawPreviewRects(Rect qr_rect, Rect text_rect) {
+        bitmap = Bitmap.createBitmap(720, 1280, Bitmap.Config.ARGB_8888);
+        canvas = new Canvas(bitmap);
+
+        Paint qr_paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        qr_paint.setColor(Color.RED);
+        qr_paint.setStyle(Paint.Style.STROKE);
+        qr_paint.setStrokeWidth(4);
+        canvas.drawRect(qr_rect.left-10, qr_rect.top-20, qr_rect.right+10, qr_rect.bottom+10, qr_paint);
+
+        Paint text_paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        text_paint.setColor(Color.WHITE);
+        text_paint.setStyle(Paint.Style.STROKE);
+        text_paint.setStrokeWidth(4);
+        canvas.drawRect(text_rect.left-20, text_rect.top-20, text_rect.right, text_rect.bottom, text_paint);
+
+        camera_imageview.setImageBitmap(bitmap);
+    }
+
 }
